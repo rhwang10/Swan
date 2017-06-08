316263e0ee36d87e9dfe68518624ba4607236ebe
rhwang10 <robert.t.hwang@gmail.com>
2017-06-08 13:27:51 -0400

    added 2 pages
    updated html, and route file to include a homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :spotifies
+  get 'home' => 'main#home'
   # get 'home' => 'main#home'
   # get 'home/submit' => 'main#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
